0d6df29416993e0f1c1fedb434f0efdf2b2af634
fredgdaley2 <fredgd2@outlook.com>
2014-06-01 23:14:33 -0400

    Second Commit

--- JustTestingGit/Form1.vb
+++ JustTestingGit/Form1.vb
@@ -3,4 +3,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MsgBox("Hello Word!")
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("Button Two Says Hello")
+    End Sub
 End Class
