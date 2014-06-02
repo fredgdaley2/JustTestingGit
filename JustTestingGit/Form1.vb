c8c11b557900a56c99a7250c7ba372aea9ec088d
fredgdaley2 <fredgd2@outlook.com>
2014-06-01 23:55:34 -0400

    added a third button

--- JustTestingGit/Form1.vb
+++ JustTestingGit/Form1.vb
@@ -7,4 +7,8 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         MsgBox("Button Two Says Hello")
     End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MsgBox("Adding a new button")
+    End Sub
 End Class
